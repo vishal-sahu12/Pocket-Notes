c787d9f9bb6f7bc3b2379c6262f840b0e855cdd8
Vishal <sahuvishal2712da@gmail.com>
2024-05-13 00:23:01 +0530

    Image modification

--- src/Components/RightSideComponent.jsx
+++ src/Components/RightSideComponent.jsx
@@ -2,12 +2,12 @@ const RightSideComponent = () =>{
     return(
         <div className="right-side-container">
 
-                <img src="./src/assets/image-removebg-preview 1.png" alt="pocket-notes-img" />
+                <img src="../src/assets/notes.png" alt="pocket-notes-img" />
                 <h1>Pocket Notes</h1>
                 <h3>Send and receive messages without keeping your phone online. <br />Use Pocket Notes on up to 4 linked devices and 1 mobile phone</h3>    
                    
                    <footer className="footer-div">
-                    <img src="./src/assets/Vector.png" alt="encryption-img" />
+                    <img src="../src/assets/Vector.png" alt="encryption-img" />
                     <h5>end-to-end encrypted</h5>
                    </footer>
                    </div>
