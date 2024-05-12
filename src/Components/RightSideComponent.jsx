4b8ed9c53b6de0e3a7ecb945db8969d7eeaa541d
Vishal <sahuvishal2712da@gmail.com>
2024-05-13 00:41:38 +0530

    image

--- src/Components/RightSideComponent.jsx
+++ src/Components/RightSideComponent.jsx
@@ -1,8 +1,11 @@
+import React from "react"
+
+import notesImg from "../assets/notes.png"
 const RightSideComponent = () =>{
     return(
         <div className="right-side-container">
 
-                <img src="/src/assets/notes.png" alt="pocket-notes-img" />
+                <img src={notesImg} alt="" srcset="" />
                 <h1>Pocket Notes</h1>
                 <h3>Send and receive messages without keeping your phone online. <br />Use Pocket Notes on up to 4 linked devices and 1 mobile phone</h3>    
                    
